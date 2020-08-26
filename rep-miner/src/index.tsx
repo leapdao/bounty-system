9c8b8a11c0490e467fdcedda6e643a5af1b4919d
Kosta Korenkov <kosta@korenkov.net>
2020-08-26 08:34:57 +0300

    toggle decimals

--- rep-miner/src/index.tsx
+++ rep-miner/src/index.tsx
@@ -1,39 +1,46 @@
 import { Fragment, render, h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { useState, useEffect, useCallback } from 'preact/hooks';
 import getReputationPoints from './data/getReputationPoints';
 import { AccountReputation, ReputationPoint } from './types';
 import { reputationForAccounts } from './reputation';
 import formatEthereumTimestamp from './utils/formatEthereumTimestamp';
 import formatWei from './utils/formatWei';
 
-const HighlighDecimals = (number: BigInt) => {
+const HighlighDecimals = (number: BigInt, showDecimals = false) => {
   const str = number.toString();
   return <span>
     {str.slice(0, str.length - str.slice(-18).length)}
-    <span style={{ opacity: 0.4 }}>{str.slice(-18)}</span>
+    {showDecimals && <span style={{ opacity: 0.4 }}>{str.slice(-18)}</span>}
   </span>
 };
-
-const renderRepPoint = (p: ReputationPoint) =>
-  <tr>
-    <td style={{ textAlign: 'right', fontSize: '80%' }}>
-      <a href={`https://etherscan.io/tx/${p.id.split('-')[0]}`} style={{ textDecoration: 'none' }}>
-       {formatEthereumTimestamp(p.timestamp)}
-      </a>
-    </td>
-    <td>{formatWei(p.reputation || '0')}{' '}
-      <span style={{ fontSize: '80%', color: '#77838f' }}>of {formatWei(p.amount)}</span>
-    </td>
-  </tr>;
 
 const App = () => {
   const [reputationPoints, setReputationPoints] = useState<AccountReputation[] | null>(null);
   const [expanded, setExpanded] = useState<string>("");
+  const [showDecimals, setShowDecimals] = useState<boolean>(false);
   useEffect(() => {
     getReputationPoints().then(repPoints =>
       setReputationPoints(reputationForAccounts(repPoints))
     );
   }, []);
+
+  const renderRepPoint = useCallback(
+    (p: ReputationPoint) =>
+      <tr>
+        <td style={{ textAlign: 'right', fontSize: '80%' }}>
+          <a href={`https://etherscan.io/tx/${p.id.split('-')[0]}`} style={{ textDecoration: 'none' }}>
+          {formatEthereumTimestamp(p.timestamp)}
+          </a>
+        </td>
+        <td style={{ textAlign: 'right' }}>
+          {HighlighDecimals(p.reputation || BigInt('0'), showDecimals)}
+        </td>
+        <td style={{ fontSize: '80%', color: '#77838f', whiteSpace: 'nowrap' }}>
+        of {formatWei(p.amount)} DAI
+        </td>
+      </tr>,
+   [showDecimals]
+  );
 
   const toggleExpanded = (account: string) =>
     setExpanded(expanded === account ? "" : account);
@@ -46,6 +53,7 @@ const App = () => {
       <div>
         <p>Once you get a payout of X DAI you receive the same amount of reputation ("reputation point"). Your reputation point decays exponentially then with such a speed that it is reduced by half every 90 days. Your org reputation is the sum of your decayed reputation points.</p>
         <p>You can click the record to reveal all the individual payouts and the corresponding reputation accrued.</p>
+        <input type="checkbox" checked={showDecimals} onClick={() => setShowDecimals(!showDecimals)} /> Show decimals
       </div>
       {!reputationPoints && <div style={{ textAlign: 'center' }}>Calculating..</div>}
       {reputationPoints && (
@@ -53,12 +61,14 @@ const App = () => {
           <thead>
             <th>Account</th>
             <th>Reputation</th>
+            <th></th>
           </thead>
           {reputationPoints.map(({ account, reputation, points }) => (
             <Fragment>
               <tr style={{ cursor: 'pointer' }} onClick={() => toggleExpanded(account)}>
                 <td>{account}</td>
-                <td style={{ textAlign: 'right' }}>{HighlighDecimals(reputation)}</td>
+                <td style={{ textAlign: 'right' }}>{HighlighDecimals(reputation, showDecimals)}</td>
+                <td></td>
               </tr>
               <tbody style={{ display: showExpandable(account) }}>
                 {points.
@@ -69,7 +79,7 @@ const App = () => {
         </table>
       )}
     </div>
-  )s;
+  );
 };
 
 render(<App />, document.getElementById('app'))
